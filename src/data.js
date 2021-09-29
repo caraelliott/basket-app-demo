c97d33045934735756aec048c41b940840043274
caraelliott <62650005+caraelliott@users.noreply.github.com>
2021-09-29 13:27:22 +0100

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,23 @@
+const data = {
+  products: [
+    {
+      id: '1',
+      name: 'Takkun',
+      price: 1400,
+      image: 'https://avatars.githubusercontent.com/u/1163735?v=4',
+    },
+    {
+      id: '2',
+      name: 'Snowball II',
+      price: 2400,
+      image: 'https://i.pinimg.com/736x/82/43/e5/8243e5b811eb61a5121aad9c054462be.jpg',
+    },
+    {
+      id: '3',
+      name: 'Cringer',
+      price: 1000,
+      image: 'https://i.gifer.com/fetch/w500-preview/66/665bc8fbfd1f3e7b681f9b9a961dee09.gif',
+    },
+  ],
+};
+export default data;
