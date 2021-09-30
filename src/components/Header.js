0b5bcce3e105538b460465073cbbbdcc44d2c02c
caraelliott <62650005+caraelliott@users.noreply.github.com>
2021-09-30 12:12:58 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,18 +8,7 @@ export default function Header(props) {
           <h1>Cats 4 Lyfe Store</h1>
         </a>
       </div>
-     /
-    /* <div>
-        <a href="#/cart">
-          Cart{' '}
-          {props.countCartItems ? (
-            <button className="badge">{props.countCartItems}</button>
-          ) : (
-            ''
-          )}
-        </a>{' '}
-        <a href="#/signin"> SignIn</a>
-      </div> */
+     
     </header>
   );
 }
